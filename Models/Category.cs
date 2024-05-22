c880d705b2810d88be2fde05de0053bb6b3f0d38
Pedro Henrique <phlgasquez@gmail.com>
2024-05-21 21:32:45 -0300

    fetch: Map models

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o titulo")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+    [MaxLength(80, ErrorMessage = "A categoria deve ter menos que 80 caracteres")]
     public string Title { get; set; } = string.Empty;
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorShop.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o titulo")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+    [MaxLength(80, ErrorMessage = "A categoria deve ter menos que 80 caracteres")]
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Informe o preco do produto")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
     
     public int CategoryId { get; set; }
